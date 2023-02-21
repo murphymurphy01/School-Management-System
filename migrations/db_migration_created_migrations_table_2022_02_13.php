c1dad1746587d26511dfabd43c0530433b4077db
Ismail Abdulbaki <ismailabdulbaki12@gmail.com>
2023-02-21 07:06:09 +0100

    check for correction

--- migrations/db_migration_created_migrations_table_2022_02_13.php
+++ migrations/db_migration_created_migrations_table_2022_02_13.php
@@ -2,9 +2,9 @@
 
 function up()
 {
-    $query = "
-    CREATE TABLE `school_management_system`.`migrations` (`id` INT NOT NULL AUTO_INCREMENT , `file` VARCHAR(400) NOT NULL UNIQUE, `status` VARCHAR(100) NOT NULL , `created_at` DATE NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
-    ";
+    $query = "CREATE TABLE `school_management_system`.`admission_forms` ( `form_no`VARCHAR(255) NOT NULL AUTO_INCREMENT , `receipt_no` VARCHAR(255) NOT NULL , `firstname` TEXT NOT NULL , `middlename` TEXT NOT NULL , `lastname` TEXT NOT NULL , `picture` TEXT NOT NULL , `residential_address` TEXT NOT NULL , `date_of_birth` DATE NOT NULL , `nationality` TEXT NOT NULL , `age` TEXT NOT NULL , `sex` TEXT NOT NULL , `religion` TEXT NOT NULL , `state_of_origin` TEXT NOT NULL , `lga` TEXT NOT NULL , `last_class_attended` TEXT NOT NULL , `class_applied_for` INT NOT NULL , `last_school_attended` TEXT NULL , `date_of_leaving` DATE NOT NULL , `transfer_cert_num` INT NULL , `school_choice` ENUM(day/boarding) NOT NULL , `hobbies` TEXT NOT NULL , `guardian_details` JSON NOT NULL , PRIMARY KEY (`form_no`)) ENGINE = InnoDB;
+    
+    
 
     return $query;
 }
@@ -13,8 +13,9 @@ function up()
 function down()
 {
     $query = "
-    DROP TABLE `school_management_system`.`migrations`
-    ";
+    
 
     return $query;
 }
+
+sql is giving me error
